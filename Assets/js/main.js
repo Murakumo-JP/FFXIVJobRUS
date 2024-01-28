611fd089725f7f160ca951ad1e0add68fe179641
Murakumo <32967450+Murakumo-JP@users.noreply.github.com>
2024-01-28 22:29:20 +0300

    Fix [PathNotes js]

--- Assets/js/main.js
+++ Assets/js/main.js
@@ -2,7 +2,7 @@
 $("#jq_tank").load("RoleActions/Tank.html")
 $("#jq_healer").load("RoleActions/Healer.html")
 
-$("#jq_patch").load("RoleActions/Path.html")
+$("#jq_patch").load("PathNotes/Path.html")
 // Back to Top
 function backToTop() {
 	let button = $(".nome_app_top")
