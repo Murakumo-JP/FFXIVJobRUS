91eef2f53421755c762f3505d3bdc7d6e881d7c6
Murakumo <32967450+Murakumo-JP@users.noreply.github.com>
2024-01-28 23:11:18 +0300

    Update Preloader

--- Assets/js/main.js
+++ Assets/js/main.js
@@ -1,8 +1,22 @@
-// Role Actions
+// Doom
 $("#jq_tank").load("RoleActions/Tank.html")
 $("#jq_healer").load("RoleActions/Healer.html")
-
-$("#jq_patch").load("PathNotes/Path.html")
+$("#jq_patch").load("Other/Path.html")
+/* Preloader */
+document.body.onload = function() {
+	setTimeout(function(){
+		var preloader = document.getElementById("page-preloader");
+		var loader = document.getElementById("page-loader");
+		if(!preloader.classList.contains("done"))
+		{
+			preloader.classList.add("done");
+			if(!loader.classList.contains("loader_none"))
+			{
+				loader.classList.add("loader_none");
+			}
+		}
+	}, 1500);
+}
 // Back to Top
 function backToTop() {
 	let button = $(".nome_app_top")
