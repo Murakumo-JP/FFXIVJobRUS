3065c1d9267a5c8d6c5695e96443c19e6435c934
Murakumo <32967450+Murakumo-JP@users.noreply.github.com>
2024-01-30 14:08:15 +0300

    Update [Quick Skil lMenu All Job]

--- Assets/js/main.js
+++ Assets/js/main.js
@@ -1,51 +1,53 @@
 // Doom
-$("#jq_tank").load("RoleActions/Tank.html")
-$("#jq_healer").load("RoleActions/Healer.html")
-$("#jq_meleeDPS").load("RoleActions/MeleeDPS.html")
-$("#jq_patch").load("Other/Path.html")
+$('#jq_tank').load('RoleActions/Tank.html');
+$('#jq_healer').load('RoleActions/Healer.html');
+$('#jq_meleeDPS').load('RoleActions/MeleeDPS.html');
+$('#jq_patch').load('Other/Path.html');
 /* Preloader */
-document.body.onload = function() {
-	setTimeout(function(){
-		var preloader = document.getElementById("page-preloader");
-		var loader = document.getElementById("page-loader");
-		if(!preloader.classList.contains("done"))
-		{
-			preloader.classList.add("done");
-			loader.classList.add("loader_none");
+document.body.onload = function () {
+	setTimeout(function () {
+		var preloader = document.getElementById('page-preloader');
+		var loader = document.getElementById('page-loader');
+		if (!preloader.classList.contains('done')) {
+			preloader.classList.add('done');
+			loader.classList.add('loader_none');
 			$('body').css('overflow', 'visible');
 		}
 	}, 1500);
-}
+};
 // Smooth Scrolling
 var $page = $('html, body');
-$('a[href*="#"]').click(function() {
-    $page.animate({
-        scrollTop: $($.attr(this, 'href')).offset().top
-    }, 1500);
-    return false;
+$('a[href*="#"]').click(function () {
+	$page.animate(
+		{
+			scrollTop: $($.attr(this, 'href')).offset().top,
+		},
+		1500
+	);
+	return false;
 });
 // Open JobMenu
 $(() => {
-	$(".nav_floating_icon").click(() => {
-		 $(".nav_floating_list").slideToggle();
+	$('.nav_floating_icon').click(() => {
+		$('.nav_floating_list').slideToggle();
 	});
 });
 // Back to Top
 function backToTop() {
-	let button = $(".nome_app_top")
- 
-	$(window).on("scroll", () => {
-	  if ($(this).scrollTop() >= 200) {
-		 button.fadeIn()
-	  } else {
-		 button.fadeOut()
-	  }
-	})
- 
-	button.on("click", e => {
-	  e.preventDefault()
-	  $("html").animate({ scrollTop: 0 }, 1000)
-	})
- }
- 
- backToTop()
+	let button = $('.nome_app_top');
+
+	$(window).on('scroll', () => {
+		if ($(this).scrollTop() >= 200) {
+			button.fadeIn();
+		} else {
+			button.fadeOut();
+		}
+	});
+
+	button.on('click', (e) => {
+		e.preventDefault();
+		$('html').animate({ scrollTop: 0 }, 1000);
+	});
+}
+
+backToTop();
