a257e50526a757eacbefde71f49c98663f10f8de
Murakumo <32967450+Murakumo-JP@users.noreply.github.com>
2024-04-26 01:08:44 +0300

    GoldSaucer [Dev v0.4]

--- Assets/js/main.js
+++ Assets/js/main.js
@@ -76,22 +76,3 @@ function backToTop() {
 }
 
 backToTop();
-
-// SVG
-
-$('img.img-svg').each(function(){
-	const $img = $(this);
-	const imgClass = $img.attr('class');
-	const imgURL = $img.attr('src');
-	$.get(imgURL, function(data) {
-	  let $svg = $(data).find('svg');
-	  if(typeof imgClass !== 'undefined') {
-		 $svg = $svg.attr('class', imgClass+' replaced-svg');
-	  }
-	  $svg = $svg.removeAttr('xmlns:a');
-	  if(!$svg.attr('viewBox') && $svg.attr('height') && $svg.attr('width')) {
-		 $svg.attr('viewBox', '0 0 ' + $svg.attr('height') + ' ' + $svg.attr('width'))
-	  }
-	  $img.replaceWith($svg);
-	}, 'xml');
- });
