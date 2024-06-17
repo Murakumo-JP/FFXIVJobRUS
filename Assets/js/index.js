7e25728ad0d0682543b4c335e6b45264ec830859
Murakumo <32967450+Murakumo-JP@users.noreply.github.com>
2024-06-17 13:31:46 +0300

    Fix style btn link and news

--- Assets/js/index.js
+++ Assets/js/index.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
    fetch('https://api.github.com/repos/Murakumo-JP/FFXIVJobGuideRU/issues/3/comments')
-   .then(function(response){
-      response.text().then(function(json){
+   .then(function (response) {
+      response.text().then(function (json) {
          let news = JSON.parse(json);
          let html = '';
          let news_max_count = 4;
@@ -9,19 +9,19 @@ $(document).ready(function () {
          for (let i = news.length - 1; i >= news.length - news_count; i--) {
             let body = '';
             let n = /## (.*)\r\n(.*)/g.exec(news[i].body);
-            if(n !== null){
+            if (n !== null) {
                body = n[1];
             } else {
                body = news[i].body.replaceAll('\r\n', '');
             }
-            let date = new Date(news[i].updated_at).toLocaleDateString(); 
-            html += '<div><span>'+ date + '</span><a target="_blank" href="'+news[i].html_url+'">'+body+'</a></div>';
+            let date = new Date(news[i].created_at).toLocaleDateString();
+            html += '<div><span>' + date + ' -</span><a target="_blank" href="' + news[i].html_url + '">' + body + '</a></div>';
          }
          document.getElementById('main_news').innerHTML = html;
       });
    })
-   .catch(function(reason){
+   .catch(function (reason) {
       console.log(reason);
       document.getElementById('main_news').innerHTML = '';
-   })
+   });
 });
