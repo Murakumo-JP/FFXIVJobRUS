d1cfcd8bd0ed058d41f0474fc72e60316c8945e8
Murakumo <32967450+Murakumo-JP@users.noreply.github.com>
2024-06-17 16:18:34 +0300

    fix

--- Assets/js/main.js
+++ Assets/js/main.js
@@ -13,8 +13,8 @@ $(document).ready(function () {
 		$('.nav_floating_list').fadeToggle();
 	});
 	// Info Update
-	$('#inner_update').prepend('<p class="latest_update">Последнее обновление: 28.06.2024 | Патч: 7.0</p>');
-	$('#jq_patch').prepend('Все описания основаны на активных умениях и бонусах, полученных на 100 уровне.<br />Более подробную информацию об изменениях в активных и пассивных умениях можно найти в примечаниях к <a target="_blank" href="https://eu.finalfantasyxiv.com/lodestone/topics/detail/1dcbf39c97285ba9a42012eecf2c031f0ffbceb1/">патч ноутам</a>.');
+	$('#inner_update').prepend('<p class="latest_update">Последнее обновление: 02.10.2023 | Патч: 6.5</p>');
+	$('#jq_patch').prepend('Все описания основаны на активных умениях и бонусах, полученных на 90 уровне.<br />Более подробную информацию об изменениях в активных и пассивных умениях можно найти в примечаниях к <a href="https://eu.finalfantasyxiv.com/lodestone/topics/detail/1dcbf39c97285ba9a42012eecf2c031f0ffbceb1/">патч ноутам</a>.');
 	$('.job_skil_list, .warn_info').prepend('<div class="error_info"><h5>Важная информация!</h5><p>Обновление сайта и перевод будет производиться по мере актуализации информации на официальном сайте игры и может занять какое-то время.</p></div>');
 	$('.SE').append('<p>All images on the site are the property of SQUARE ENIX© and are used under the <a href="https://support.na.square-enix.com/rule.php?id=5382&tag=authc">Materials Usage License</a></p>')
 });
