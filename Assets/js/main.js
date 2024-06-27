a0275e043d1b6a56e5101f48de5c71e005b1c2eb
Murakumo <32967450+Murakumo-JP@users.noreply.github.com>
2024-06-28 00:31:05 +0300

    Update DB BRD

--- Assets/js/main.js
+++ Assets/js/main.js
@@ -1,16 +1,18 @@
 // Doom Load
 $(document).ready(function () {
 	$('#jq_tank').load('RoleActions/Tank.html');
-	$('#jq_tank_traits').load('RoleActions/TankRoleTraits.html');
 	$('#jq_healer').load('RoleActions/Healer.html');
-	$('#jq_healer_traits').load('RoleActions/HealerRoleTraits.html');
 	$('#jq_meleeDPS').load('RoleActions/MeleeDPS.html');
-	$('#jq_melee_traits').load('RoleActions/MeleeRoleTraits.html');
 	$('#jq_physicalDPS').load('RoleActions/PhysicalDPS.html');
 	$('#jq_magicalDPS').load('RoleActions/MagicalDPS.html');
 	$('#jq_menu').load('Other/MenuJob.html');
 	$('#jq_menu_cg').load('Other/MenuCG.html');
 	$('#jq_actions').load('Other/Actions.html');
+	// Role Traits
+	$('#jq_tank_traits').load('RoleActions/TankRoleTraits.html');
+	$('#jq_healer_traits').load('RoleActions/HealerRoleTraits.html');
+	$('#jq_melee_traits').load('RoleActions/MeleeRoleTraits.html');
+	$('#jq_physical_traits').load('RoleActions/PhysicalRoleTraits.html');
 	// Open JobMenu
 	$('.nav_floating_icon').click(() => {
 		$('.nav_floating_list').fadeToggle();
